421a70880f67e4bdf5d134c80b7d5ca04d4ad2fa
staffscollab2020-G2 <staffscollab.2020.g2@gmail.com>
2020-05-19 22:30:40 +0100

    Childed Surface detail effect to Base effect as well.

--- Tutorial01/d_SurfaceDetailEffect.h
+++ Tutorial01/d_SurfaceDetailEffect.h
@@ -1,14 +1,13 @@
 #pragma once
 
-#include "d_Utilities.h"
-#include "structures.h"
+#include "d_BaseFX.h"
 
-class SurfaceDetailFX
+class SurfaceDetailFX : public BaseFX
 {
 private:
-	ID3D11VertexShader* _pVertexShader;
+	/*ID3D11VertexShader* _pVertexShader;
 	ID3D11PixelShader* _pPixelShader;
-	ID3D11InputLayout* _pVertexLayout;
+	ID3D11InputLayout* _pVertexLayout;*/
 	ID3D11Buffer* _pConstantBuffer;
 	ID3D11Buffer* _pLightBuffer;
 	ID3D11Buffer* _pMaterialBuffer;
@@ -17,15 +16,17 @@ private:
 	static constexpr int TEX_NUM = 3;
 	ID3D11ShaderResourceView** _pTextureRVs;
 
+protected:
+	HRESULT CreateShaders(ID3D11Device* pd3dDevice);
+	HRESULT CreateBuffers(ID3D11Device* pd3dDevice);
+	HRESULT CreateTextures(ID3D11Device* pd3dDevice);
 
 public:
 	SurfaceDetailFX();
 	~SurfaceDetailFX();
 
-	HRESULT SetupShader(ID3D11Device* pd3dDevice);
-	HRESULT CreateShaders(ID3D11Device* pd3dDevice);
-	HRESULT CreateBuffers(ID3D11Device* pd3dDevice);
-	HRESULT CreateTextures(ID3D11Device* pd3dDevice);
+	//HRESULT SetupShader(ID3D11Device* pd3dDevice);
+	
 	void SetConstantBuffers(ID3D11DeviceContext* pContext, 
 		DirectX::XMMATRIX* world, DirectX::XMMATRIX* view, DirectX::XMMATRIX* proj, 
 		const DirectX::XMFLOAT4& lightPos, const DirectX::XMFLOAT4& camPos, const Light& light, int effect);
